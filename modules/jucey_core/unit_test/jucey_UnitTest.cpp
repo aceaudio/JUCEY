8b8f11086604f3c3765b3ef5e08891b409366157
Anthony Nicholls <anicholls@outlook.com>
2016-07-28 01:25:06 +0100

    Dropped default argument to expectMessage macros due to issues on Windows

--- modules/jucey_core/unit_test/jucey_UnitTest.cpp
+++ modules/jucey_core/unit_test/jucey_UnitTest.cpp
@@ -10,51 +10,38 @@ public:
     {
         //======================================================================
         beginTest ("expectMessageLogged");
-
-        expectMessageLogged (juce::Logger::writeToLog ("abc123"), "abc123");
         expectMessageLogged (juce::Logger::writeToLog ("abc123"), "abc123", "failure message");
         
         //======================================================================
         beginTest ("expectMessageNotLogged");
-        
-        expectMessageNotLogged (juce::Logger::writeToLog ("123abc"), "abc123");
         expectMessageNotLogged (juce::Logger::writeToLog ("123abc"), "abc123", "failure message");
         
         //======================================================================
         beginTest ("expectMessageLoggedStartingWith");
-
-        expectMessageLoggedStartingWith (juce::Logger::writeToLog ("abc123"), "abc");
         expectMessageLoggedStartingWith (juce::Logger::writeToLog ("abc123"), "abc", "failure message");
         
         //======================================================================
         beginTest ("expectMessageNotLoggedStartingWith");
-        
-        expectMessageNotLoggedStartingWith (juce::Logger::writeToLog ("abc"), "123");
         expectMessageNotLoggedStartingWith (juce::Logger::writeToLog ("abc"), "123", "failure message");
         
         //======================================================================
         beginTest ("expectJassert");
-        
         expectJassert (jassertfalse);
         
         //======================================================================
         beginTest ("expectJassertIn: jucey_UnitTest.cpp");
-        
         expectJassertIn (jassertfalse, "jucey_UnitTest.cpp");
         
         //======================================================================
         beginTest ("expectDoesNotJassert");
-        
         expectDoesNotJassert (jassert (true));
         
         //======================================================================
         beginTest ("expectDoesNotJassertIn: jucey_UnitTest.cpp");
-        
         expectDoesNotJassertIn (jassert (true), "jucey_UnitTest.cpp");
         
         //======================================================================
         beginTest ("expectDoesNotJassertIn: jucey_Other.cpp");
-        
         expectDoesNotJassertIn (jassertfalse, "jucey_Other.cpp");
     }
 };
--- modules/jucey_core/unit_test/jucey_UnitTest.h
+++ modules/jucey_core/unit_test/jucey_UnitTest.h
@@ -4,28 +4,28 @@
 
 #if JUCE_LOG_ASSERTIONS
 
-    #define expectMessageLogged_3(expr, text, failureMessage)               \
+    #define expectMessageLogged(expr, text, failureMessage)                 \
         {                                                                   \
             jucey::ScopedLogger scopedLogger;                               \
             {expr;}                                                         \
             expect (scopedLogger.containsMessage (text), failureMessage);   \
         }
 
-    #define expectMessageNotLogged_3(expr, text, failureMessage)                \
+    #define expectMessageNotLogged(expr, text, failureMessage)                  \
         {                                                                       \
             jucey::ScopedLogger scopedLogger;                                   \
             {expr;}                                                             \
             expect ( ! scopedLogger.containsMessage (text), failureMessage);    \
         }
 
-    #define expectMessageLoggedStartingWith_3(expr, text, failureMessage)               \
+    #define expectMessageLoggedStartingWith(expr, text, failureMessage)                 \
         {                                                                               \
             jucey::ScopedLogger scopedLogger;                                           \
             {expr;}                                                                     \
             expect (scopedLogger.containsMessageStartingWith (text), failureMessage);   \
         }
 
-    #define expectMessageNotLoggedStartingWith_3(expr, text, failureMessage)                \
+    #define expectMessageNotLoggedStartingWith(expr, text, failureMessage)                  \
         {                                                                                   \
             jucey::ScopedLogger scopedLogger;                                               \
             {expr;}                                                                         \
@@ -39,55 +39,19 @@
         definitions.
      */
 
-    #define expectMessageLogged_3(expr, text, failureMessage) \
+    #define expectMessageLogged(expr, text, failureMessage) \
         static_jassert (false)
 
-    #define expectMessageNotLogged_3(expr, text, failureMessage) \
+    #define expectMessageNotLogged(expr, text, failureMessage) \
         static_jassert (false)
 
-    #define expectMessageLoggedStartingWith_3(expr, text, failureMessage) \
+    #define expectMessageLoggedStartingWith(expr, text, failureMessage) \
         static_jassert (false)
 
-    #define expectMessageNotLoggedStartingWith_3(expr, text, failureMessage) \
+    #define expectMessageNotLoggedStartingWith(expr, text, failureMessage) \
         static_jassert (false)
 
 #endif // JUCE_LOG_ASSERTIONS
-
-#if JUCE_WINDOWS
-    #define JUCEY_EXPAND_VA_ARGS(x) JUCEY_EXPAND_VA_ARGS_HELPER (x)
-    #define JUCEY_EXPAND_VA_ARGS_HELPER(x) x
-#else // JUCE_WINDOWS
-    #define JUCEY_EXPAND_VA_ARGS(...) __VA_ARGS__
-#endif // JUCE_WINDOWS
-
-#define JUCEY_COUNT_ARGS_HELPER(_10, _9, _8, _7, _6, _5, _4, _3, _2, _1, N, ...) N
-
-#define JUCEY_COUNT_ARGS(...) \
-    JUCEY_COUNT_ARGS_HELPER (JUCEY_EXPAND_VA_ARGS (__VA_ARGS__), 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0)
-
-#define expectMessageLogged_2(expr, text) \
-    expectMessageLogged_3 (expr, text, juce::String())
-
-#define expectMessageNotLogged_2(expr, text) \
-    expectMessageNotLogged_3 (expr, text, juce::String())
-
-#define expectMessageLoggedStartingWith_2(expr, text) \
-    expectMessageLoggedStartingWith_3 (expr, text, juce::String())
-
-#define expectMessageNotLoggedStartingWith_2(expr, text) \
-    expectMessageNotLoggedStartingWith_3 (expr, text, juce::String())
-
-#define expectMessageLogged(...) \
-    JUCE_JOIN_MACRO (expectMessageLogged_, JUCEY_COUNT_ARGS (JUCEY_EXPAND_MACRO (__VA_ARGS__))) (__VA_ARGS__)
-
-#define expectMessageNotLogged(...) \
-    JUCE_JOIN_MACRO (expectMessageNotLogged_, JUCEY_COUNT_ARGS(__VA_ARGS__)) (__VA_ARGS__)
-
-#define expectMessageLoggedStartingWith(...) \
-    JUCE_JOIN_MACRO (expectMessageLoggedStartingWith_, JUCEY_COUNT_ARGS(__VA_ARGS__)) (__VA_ARGS__)
-
-#define expectMessageNotLoggedStartingWith(...) \
-    JUCE_JOIN_MACRO (expectMessageNotLoggedStartingWith_, JUCEY_COUNT_ARGS(__VA_ARGS__)) (__VA_ARGS__)
 
 #define expectJassert(expr) \
     expectMessageLoggedStartingWith (expr, "JUCE Assertion failure in ", \
